375a78c73f0d61e34d6a0d01d2aeca4fcf457e47
Daimyo <daimyo21mods@gmail.com>
2013-03-16 21:57:08 -0700

    Deployable inserts for database.

---
+++ basebuilding/database/BaseBuilding_Deployables.sql
@@ -0,0 +1,54 @@
+/*
+-- Query: SELECT * FROM dayz_tavi2.deployable
+LIMIT 0, 1000
+
+-- Date: 2013-03-16 14:34
+*/
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (9,'Base_WarfareBBarrier10x');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (7,'Base_WarfareBBarrier10xTall');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (41,'CamoNet');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (38,'Concrete_Wall_EP1');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (42,'DeerStand');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (22,'Fence_corrugated_plate');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (33,'Fence_Ind');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (31,'Fence_Ind_long');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (32,'Fort_RazorWire');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (52,'Gate1_DZ');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (35,'Grave');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (47,'HBarrier');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (3,'Hedgehog_DZ');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (19,'Hhedgehog_concrete');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (39,'Infostand_2_EP1');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (17,'Ins_WarfareBContructionSite');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (51,'LadderSmall');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (40,'Land_BagFenceRound');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (25,'Land_CamoNetB_EAST');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (26,'Land_CamoNetB_NATO');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (27,'Land_CamoNetVar_EAST');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (28,'Land_CamoNetVar_NATO');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (29,'Land_CamoNet_EAST');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (30,'Land_CamoNet_NATO');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (2,'Land_CncBlock');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (10,'Land_fortified_nest_big');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (14,'Land_fortified_nest_small');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (34,'Land_fort_bagfence_long');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (12,'Land_fort_rampart_EP1');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (11,'Land_Fort_Watchtower');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (13,'Land_HBarrier_large');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (15,'Land_Misc_Cargo2E');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (21,'Land_prebehlavka');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (18,'Land_pumpa');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (24,'Land_tent_east');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (16,'Misc_Cargo1Bo_military');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (20,'Misc_cargo_cont_small_EP1');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (49,'Sandbag2_DZ');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (50,'Sandbag3_DZ');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (45,'SandBagNest');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (48,'Scaffolding');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (1,'TentStorage');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (8,'WarfareBCamp');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (6,'WarfareBDepot');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (44,'WatchTower');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (46,'Wire2');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (43,'Wire_cat1');
+INSERT INTO `deployable` (`id`,`class_name`) VALUES (23,'ZavoraAnim');
